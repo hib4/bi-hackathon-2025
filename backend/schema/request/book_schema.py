49fdde42bd6c3e76bb00664b091b5ade438e08f3
jamesalv <jamesalvdhan@gmail.com>
2025-07-24 20:36:54 +0700

    fix: set default value for voice_name_code in create_book_schema

--- backend/schema/request/book_schema.py
+++ backend/schema/request/book_schema.py
@@ -15,7 +15,7 @@ class language_enum(str, Enum):
 class create_book_schema(BaseModel):
     query: str
     age: int
-    voice_name_code: str
+    voice_name_code: str = "en-US-JennyMultilingualNeural"
 
 class get_book_by_id_schema(BaseModel):
     id: str
